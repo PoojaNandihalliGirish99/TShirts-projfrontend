313b80b06bf109908492003668e9aa2d1b20038b
Pooja N G <PoojaNandihalliGirish99@users.noreply.github.com>
2021-05-01 10:41:11 +0530

    Navbar and footer

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import "../styles.css"
+import {API} from "../backend.js"
+import Base from "./Base"
 
 export default function Home() {
+    console.log("API IS ", API)
     return(
-        <div>
-            <h1 className="text-white">Hello Front end</h1>
+        <Base title="Home Page">
+        <div className="row">
+        <div className="col-4">
+        <button className="btn btn-success">TEST</button>
         </div>
+        <div className="col-4">
+        <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+        <button className="btn btn-success">TEST</button>
+        </div>
+        </div>
+        </Base>
     )
 }
