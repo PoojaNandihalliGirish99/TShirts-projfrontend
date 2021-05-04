bcd1634aa8de72bdfbc569f6c5fb2a360340eb9e
Pooja N G <PoojaNandihalliGirish99@users.noreply.github.com>
2021-05-04 08:08:20 +0530

    signup and signin

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,45 +1,77 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {Link , withRouter} from "react-router-dom"
+import { signout, isAuthenticated } from '../auth/helper'
 
-const Menu = () => {
+
+const currentTab = (history, path) => {
+    if(history.location.pathname === path) {
+        return {color: "#2ecc72"}
+    } else {
+        return {color: "#ffffff"}
+    }
+}
+
+const Menu = ({history}) => {
     return (
         <div>
         <ul className="nav nav-tabs bg-dark">
         <li className="nav-item">
-        <Link className="nav-link" to="/">
+        <Link style={currentTab(history, "/")}
+        className="nav-link" to="/">
         Home
         </Link>
         </li>
         <li className="nav-item">
-        <Link className="nav-link" to="/">
+        <Link style={currentTab(history, "/cart")}
+        className="nav-link" to="/cart">
         Cart
         </Link>
         </li>
         <li className="nav-item">
-        <Link className="nav-link" to="/">
-        Dashboard
+        <Link style={currentTab(history, "/user/dashboard")}
+        className="nav-link" to="/user/dashboard">
+        U.Dashboard
         </Link>
         </li>
         <li className="nav-item">
-        <Link className="nav-link" to="/">
+        <Link style={currentTab(history, "/admin/dashboard")}
+        className="nav-link" to="/admin/dashboard">
         A.Dashboard
         </Link>
         </li>
+        {!isAuthenticated() && (
+
+            <Fragment>
         <li className="nav-item">
-        <Link className="nav-link" to="/">
+        <Link style={currentTab(history, "/signin")}
+        className="nav-link" to="/signin">
         Sign In
         </Link>
         </li>
         <li className="nav-item">
-        <Link className="nav-link" to="/">
+        <Link style={currentTab(history, "/signup")}
+        className="nav-link" to="/signup">
         Signup
         </Link>
         </li>
+        </Fragment>
+
+        )}
+        {isAuthenticated() && (
         <li className="nav-item">
-        <Link className="nav-link" to="/">
-        Signout
-        </Link>
+            <span
+            className="nav-link text-warning"
+            onClick={() => {
+                signout(()=> {
+                    history.push("/")
+                })
+
+            }}>
+                Signout
+            </span>    
         </li>
+
+        )}
         </ul>
         </div>
 
